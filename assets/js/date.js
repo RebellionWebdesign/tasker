8b8f413212da41d47bb17824d53a7ac0aa41da33
Daniel Stauffer <info@rebellion-webdesign.com>
2023-07-24 07:40:18 +0200

    Update function to fire when DOM content is loaded

--- assets/js/date.js
+++ assets/js/date.js
@@ -7,13 +7,14 @@
 *
 */
 
+const day = ["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"];
+const newDay = new Date();
+let dayOfWeek = day[newDay.getDay()];
+let currentDate = new Date().toLocaleDateString("de-DE");
+
 function date() {
-    const day = ["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"];
-    const newDay = new Date();
-    let dayOfWeek = day[newDay.getDay()];
-
-    let currentDate = new Date().toLocaleDateString("de-DE");
-
     document.getElementById("day").innerHTML = dayOfWeek;
     document.getElementById("date").innerHTML = currentDate;
 }
+
+document.addEventListener("DOMContentLoaded", date())
